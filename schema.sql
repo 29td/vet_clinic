b9088ee280de8cbd601cf4e7132055fac9041cbc
29td <nonhlanhlamndebele9@gmail.com>
2022-09-21 20:16:30 +0200

    add screenshots

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id int IDENTITY(1,1),
+    id INTEGER PRIMARY KEY NOT NULL,
     name varchar(50),
     date_of_birth DATE,
     escape_attempts INTEGER,
