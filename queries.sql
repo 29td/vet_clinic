542cbd8877a21a505856fe3e18dd998d256553a3
29td <nonhlanhlamndebele9@gmail.com>
2022-09-22 06:57:11 +0200

    change birthdate to date of birth

--- queries.sql
+++ queries.sql
@@ -8,3 +8,41 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+SELECT * FROM animals;
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+SELECT * FROM animals;
+UPDATE animals SET species = 'Pokemon' WHERE species is NULL;
+COMMIT;
+SELECT * FROM animals;
+BEGIN;
+DELETE FROM animals; 
+ROLLBACK;
+SELECT * FROM animals;
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT S1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+ROLLBACK TO S1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1 
+WHERE weight_kg < 0;
+
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT MAX(escape_attempts), neutered FROM animals GROUP BY neutered;
+
+SELECT Min(weight_kg), Max(weight_kg), COUNT(weight_kg), species FROM animals GROUP BY species;
+
+SELECT AVG(escape_attempts), species, date_of_birth FROM animals GROUP BY species, date_of_birth HAVING date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
